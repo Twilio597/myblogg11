3d345588a149375802bb0992a32d43776e02d200
Somesh <somshptl@gmail.com>
2024-04-16 13:14:01 +0530

    127:Create save Post Feature

---
+++ src/main/java/com/myblogg/myblogg11/Myblogg11Application.java
@@ -0,0 +1,13 @@
+package com.myblogg.myblogg11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myblogg11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myblogg11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/myblogg/myblogg11/Myblogg11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myblogg.myblogg11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myblogg11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
